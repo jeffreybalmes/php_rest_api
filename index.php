c17cca9743c96dc77a1779e36b531738a72c6efe
jeffreybalmes <jefbalmes@gmail.com>
2018-10-24 23:05:49 +0800

    Database & Read

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+   include_once 'config/Database.php';
+   include_once 'models/Post.php';
+
+   // Instantiate DB & connect
+   $database = new Database();
+   $db = $database->connect();
+
+   // Instantiate blog post object
+   $post = new Post($db);
+
+   // Blog post query
+   $result = $post->read();
+   // Get row count
+   $num = $result->rowCount();
+
+   // Check if any posts
+   if ($num > 0) {
+
+      // Post array
+      $post_arr = array();
+
+      while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+         extract($row);
+
+         $post_item = array(
+            'id' => $id,
+            'title' => $title,
+            'body' => html_entity_decode($body),
+            'author' => $author,
+            'category_id' => $category_id,
+            'category_name' => $category_name,
+            'created_at' => $created_at
+         );
+
+         // Push to data
+         array_push($post_arr, $post_item);
+      }
+
+      // Turn to JSON & output
+      // $posts = json_encode($post_arr);
+   }
+?>
 <?php include('inc/header.php'); ?>
 <?php include('inc/navbar.php'); ?>
 
@@ -5,44 +48,24 @@
       <div class="row mt-4">
          <div class="col-md-8">
             <h2 class="mb-3">Posts</h2>
-            <div class="card mb-3">
-               <div class="card-body">
-                  <h5 class="card-title"><a href="single.html">Web Developer</a></h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-               </div>
-               <div class="card-footer">
-                  <small class="text-muted">Technology, Science, Robotics</small>
-                  <small class="text-muted float-right">Last updated 3 mins ago</small>
-               </div>
-            </div> <!-- /.card -->
-            <div class="card mb-3">
-               <div class="card-body">
-                  <h5 class="card-title"><a href="#">Web Developer</a></h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                     This content is a little bit longer.</p>
-               </div>
-               <div class="card-footer">
-                  <small class="text-muted">Technology, Science, Robotics</small>
-                  <small class="text-muted float-right">Last updated 3 mins ago</small>
-               </div>
-            </div> <!-- /.card -->
-            <div class="card mb-3">
-               <div class="card-body">
-                  <h5 class="card-title"><a href="#">Web Developer</a></h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                     This content is a little bit longer.</p>
-               </div>
-               <div class="card-footer">
-                  <small class="text-muted">Technology, Science, Robotics</small>
-                  <small class="text-muted float-right">Last updated 3 mins ago</small>
-               </div>
-            </div> <!-- /.card -->
+
+            <?php foreach($post_arr as $post): ?>
+               <div class="card mb-3">
+                  <div class="card-body">
+                     <h4 class="card-title"><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h4>
+                     <p class="card-text"><?php echo $post['body']; ?></p>
+                  </div>
+                  <div class="card-footer">
+                     <small class="text-muted"><?php echo $post['category_name']; ?></small>
+                     <small class="text-muted float-right"><?php echo $post['created_at']; ?></small>
+                  </div>
+               </div> <!-- /.card -->
+            <?php endforeach; ?>
+
             <div class="form-group text-center mt-4">
                <button class="btn btn-default">Load more...</button>
             </div>
-
          </div> <!-- /.col-md-8 -->
-
          <?php include('inc/sidebar.php'); ?>
       </div>
    </div>
