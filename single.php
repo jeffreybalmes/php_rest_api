c3ace0cd1508473769cbe44ce224c5e3d25a228c
jeffreybalmes <jefbalmes@gmail.com>
2018-10-24 23:23:44 +0800

    view single post

--- single.php
+++ single.php
@@ -1,18 +1,43 @@
+<?php
+   include_once 'config/Database.php';
+   include_once 'models/Post.php';
+
+   // Instantiate DB & connect
+   $database = new Database();
+   $db = $database->connect();
+
+   // Instantiate blog post object
+   $post = new Post($db);
+
+   // Get ID
+   $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+   // Get Post
+   $post->read_single();
+
+   // Create array
+   $post_arr = array(
+      'id' => $post->id,
+      'title' => $post->title,
+      'body' => $post->body,
+      'author' => $post->author,
+      'category_id' => $post->category_id,
+      'category_name' => $post->category_name,
+      'created_at' => $post->created_at
+   );
+?>
 <?php include('inc/header.php'); ?>
 <?php include('inc/navbar.php'); ?>
 
    <div class="container">
       <div class="row mt-4">
          <div class="col-md-8">
-				<h2>Web Developer</h2>
-				<small class="text-muted">Technology, Science, Robotics</small>
-				<p class="card-text mt-3 mb-2">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, incidunt assumenda consectetur repellat minus aspernatur deserunt qui quod esse quis enim amet mollitia ut.</p>
-				<p class="card-text mt-3 mb-2">This is a wider card with supporting text below as a natural lead-in to additional
-					content. This content is a little bit longer. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore,
-					incidunt assumenda consectetur repellat minus aspernatur deserunt qui quod esse quis enim amet mollitia ut.</p>
-				<div class="form-group mt-4">
-					<a href="index.html" class="btn btn-default">&laquo; Back</a>
-				</div>
+            <h2><?php echo $post_arr['title']; ?></h2>
+            <small class="text-muted"><?php echo $post_arr['category_name']; ?></small>
+            <p class="card-text mt-3 mb-2"><?php echo $post_arr['body']; ?></p>
+            <div class="form-group mt-4">
+               <a href="index.php" class="btn btn-default">&laquo; Back</a>
+            </div>
          </div> <!-- /.col-md-8 -->
          <div class="col-md-4">
             <div class="input-group mb-4">
