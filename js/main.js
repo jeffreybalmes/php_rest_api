0f86187af1e5729ce4411f3af8e26c92be46bc56
jeffreybalmes <31235599+jeffreybalmes@users.noreply.github.com>
2019-04-14 13:23:02 +0800

    vue init

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import App from './components/app.js';
+import adminApp from './admin-components/admin-app.js';
 
 new Vue({
    render: h => h(App),
 }).$mount('#app');
+
+new Vue({
+   render: h => h(adminApp),
+}).$mount('#admin-app');
